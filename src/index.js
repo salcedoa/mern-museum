bd6e4a442e5a01902719fc455abca78cd79ee359
salcedoa <andreas.salcedo-cespedes@city.ac.uk>
2022-11-05 00:09:22 +0000

    Set up react router for page routing

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Events from './pages/Events';
+import Collection from './pages/Collection';
+import Shop from './pages/Shop';
+import Support from './pages/Support';
+import NoPage from './pages/NoPage';
 
-const myFirstElement = <h1>Hello React!</h1>
-
-const myelement = (
-  <table>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>John</td>
-    </tr>
-    <tr>
-      <td>Elsa</td>
-    </tr>
-  </table>
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="events" element={<Events />} />
+          <Route path="collection" element={<Collection />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="support" element={<Support />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myelement);
+root.render(<App />);
